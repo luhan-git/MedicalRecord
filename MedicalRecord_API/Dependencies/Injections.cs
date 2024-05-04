f4655b1eadb9fa3c5a08665d3180f3a51196c6d5
luhan <n00209455@upn.pe>
2024-05-04 11:14:10 -0500

    fix: configurando las dependendencias / inyecciones

---
+++ MedicalRecord_API/Dependencies/Injections.cs
@@ -0,0 +1,16 @@
+﻿using MedicalRecord_API.Models;
+using MedicalRecord_API.Repository.Interfaces;
+using MedicalRecord_API.Repository.Implements;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicalRecord_API.Dependencies
+{
+    public static class Injections
+    {
+        public static void Injection(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<DbhistoriasContext>(option => option.UseMySql(configuration.GetConnectionString("Context"),ServerVersion.Parse("8.0.30-mysql")));
+            services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        }
+    }
+}
