ab95cc533a584efca18bd801e647ed3dcb4b603d
luhan <n00209455@upn.pe>
2024-05-16 23:05:02 -0500

    esquema de base de datos relacional

---
+++ MedicalRecord_API/Data/nuevo/nuevastablas.sql
@@ -0,0 +1,259 @@
+DROP DATABASE IF EXISTS DBHISTORIAS;
+CREATE DATABASE DBHISTORIAS;
+USE DBHISTORIAS;
+
+DROP TABLE IF EXISTS Usuario;
+CREATE TABLE Usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL UNIQUE,
+    correo VARCHAR(50) NOT NULL UNIQUE,
+    clave VARCHAR(250) NOT NULL,
+    cargo VARCHAR(30)  NOT NULL,
+	especialidad VARCHAR(30) NULL,
+	nroColMedico VARCHAR(6) NULL,
+    activo BOOL DEFAULT TRUE,
+    fechaRegistro DATETIME DEFAULT CURRENT_TIMESTAMP,
+    ultimaSesion DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+DROP TABLE IF EXISTS Laboratorio;
+CREATE TABLE Laboratorio (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	nombre VARCHAR(40) NOT NULL,
+	abreviatura VARCHAR(4) NULL
+);
+
+DROP TABLE IF EXISTS Presentacion;
+CREATE TABLE Presentacion (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	nombre VARCHAR(30) NOT NULL,
+	abreviatura VARCHAR(4) NULL
+);
+DROP TABLE IF EXISTS CIE;
+CREATE TABLE CIE (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	codigo VARCHAR(5) NOT NULL,
+	enfermedad VARCHAR(120) NOT NULL
+);
+
+DROP TABLE IF EXISTS ExamenLaboratorio;
+CREATE TABLE ExamenLaboratorio (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	nombre VARCHAR(50) NOT NULL,
+	abreviatura VARCHAR(20) NULL
+);
+
+DROP TABLE IF EXISTS Procedimiento;
+CREATE TABLE Procedimiento (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	nombre VARCHAR(50) NOT NULL,
+	abreviatura VARCHAR(20) NULL
+);
+
+DROP TABLE IF EXISTS CiaSeguro;
+CREATE TABLE CiaSeguro (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	nombre VARCHAR(50) NOT NULL,
+	abreviatura VARCHAR(20) NULL
+);
+DROP TABLE IF EXISTS Directorio;
+CREATE TABLE Directorio (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	nombre VARCHAR(80) NOT NULL,
+	representante VARCHAR(80) NULL,
+	telefono VARCHAR(40) NULL,
+	celular VARCHAR(40) NULL,
+	email VARCHAR(80) NULL,
+	direccion VARCHAR(180) NULL,
+	estado BOOL DEFAULT TRUE
+);
+DROP TABLE IF EXISTS Ocupacion;
+CREATE TABLE Ocupacion (
+	id INT AUTO_INCREMENT  PRIMARY KEY,
+	nombre VARCHAR(60) NOT NULL,
+	detalle VARCHAR(20) NULL
+);
+DROP TABLE IF EXISTS Medicamento;
+CREATE TABLE Medicamento (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	codigo VARCHAR(7) NOT NULL,
+	nombreComercial VARCHAR(50) NULL,
+	nombreGenerico VARCHAR(50) NULL,
+	estado VARCHAR(1) NULL,
+	dosis VARCHAR(80) NULL,
+	indicacion VARCHAR(180) NULL,
+    idPresentacion INT NOT NULL REFERENCES presentacion(id),
+    idLaboratorio INT NOT NULL REFERENCES Laboratorio(id)
+);
+DROP TABLE IF EXISTS Diabetes;
+CREATE TABLE Diabetes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tipo VARCHAR(50) NOT NULL,
+    detalle VARCHAR(100)
+);
+DROP TABLE IF EXISTS Alergia;
+CREATE TABLE Alergia (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50)
+);
+
+DROP TABLE IF EXISTS departamento;
+CREATE TABLE departamento (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  departamento varchar(50) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=26;
+
+DROP TABLE IF EXISTS provincia;
+CREATE TABLE provincia (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  provincia varchar(50) NOT NULL,
+  idDepartamento int NOT NULL REFERENCES departamento(id)
+) ENGINE=InnoDB AUTO_INCREMENT=194;
+
+DROP TABLE IF EXISTS distrito;
+CREATE TABLE distrito (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  distrito varchar(50) NOT NULL,
+  idProvincia int NOT NULL REFERENCES provincia(id)
+) ENGINE=InnoDB AUTO_INCREMENT=1833;
+
+DROP TABLE IF EXISTS Parentesco;
+CREATE TABLE Parentesco(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    valor VARCHAR(20)
+);
+
+DROP TABLE IF EXISTS Paciente;
+CREATE TABLE Paciente(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	condicion CHAR(1) NOT NULL CHECK(condicion IN('0','1','2')),
+	aPaterno VARCHAR(25) NOT NULL,
+	aMaterno VARCHAR(25) NOT NULL,
+	nombres VARCHAR(50) NOT NULL,
+	tipoDocumento CHAR(1) NOT NULL CHECK(tipoDocumento IN('0','1','2','3')),
+	numeroDocumento VARCHAR(12) NOT NULL,
+	fechaNacimiento DATETIME NOT NULL,
+    edad VARCHAR(3) NOT NULL,
+	sexo BOOL NOT NULL,
+	estadoCivil CHAR(1) NOT NULL CHECK ( estadoCivil IN('0','1','2','3','4') ),
+	grupoSanguineo VARCHAR(10) NOT NULL CHECK (grupoSanguineo IN('0','1','2','3','4','5','6','7','8')),
+	nacionalidad CHAR(1) NOT NULL CHECK ( nacionalidad IN('0','1') ),
+	idDepartamento INT NOT NULL REFERENCES departamento(id),
+	idProvincia INT NOT NULL REFERENCES provincia(id),
+	idDistrito INT NOT NULL REFERENCES distrito(id),
+	direccion VARCHAR(60) NULL,
+	telefono VARCHAR(20) NULL,
+    celular VARCHAR(20) NULL,
+	centroTrabajo VARCHAR(40) NULL,
+    asegurado BOOL DEFAULT FALSE,
+    idCiaSeguro INT NULL REFERENCES CiaSeguro(id),
+	numeroCarnet VARCHAR(10) NULL,
+	contacto VARCHAR(50) NULL,
+    idParentesco INT REFERENCES parentesco(id),
+	telefonoContacto VARCHAR(20) NULL,
+	celularContacto VARCHAR(20) NULL,
+	perfil VARCHAR(200) NULL,
+	antecedentesClinicos VARCHAR(150) NULL,
+	antecedentesFamiliares VARCHAR(150) NULL,
+	idOcupacion INT NULL REFERENCES ocupacion(id),
+	presionArterial CHAR(3) NOT NULL CHECK ( presionArterial IN('0','1','2') ),
+	campoVisual VARCHAR(6) NULL,
+	email VARCHAR(80) NULL,
+    diabetico BOOL DEFAULT FALSE,
+    idDiabetes INT NULL REFERENCES Diabetes(id),
+    alergico BOOL DEFAULT FALSE,
+    fechaCreacion DATETIME DEFAULT CURRENT_TIMESTAMP,
+    fechaActualizacion DATETIME ON UPDATE CURRENT_TIMESTAMP
+);
+DROP TABLE IF EXISTS DetalleAlergia;
+CREATE TABLE DetalleAlergia (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    idPaciente INT NOT NULL,
+    idAlergia INT NOT NULL,
+    nombre VARCHAR(50),
+    detalle VARCHAR(50)
+);
+
+DROP TABLE IF EXISTS Consulta;
+CREATE TABLE Consulta(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	numeroConsulta varchar(7),
+	motivo VARCHAR(80) NOT NULL,
+	enfermedadActual VARCHAR(200) NULL,
+	davsc VARCHAR(10) NULL,
+	iavsc VARCHAR(10) NULL,
+	davcc VARCHAR(10) NULL,
+	iavcc VARCHAR(10) NULL,
+	dpio VARCHAR(10) NULL,
+	ipio VARCHAR(10) NULL,
+    shimer VARCHAR(10) NULL,
+    valorK VARCHAR(80) NULL,
+	diagnostico VARCHAR(200) NULL,
+    idCie INT REFERENCES CIE(id),
+	idUsuario INT REFERENCES usuario(id),
+    fechaConsulta DATETIME DEFAULT CURRENT_TIMESTAMP,
+    fechaActualizacion DATETIME ON UPDATE CURRENT_TIMESTAMP
+
+);
+DROP TABLE IF EXISTS DetalleExamen;
+CREATE TABLE DetalleExamen (
+    id INT AUTO_INCREMENT PRIMARY KEY ,
+	idConsulta INT NOT NULL REFERENCES Consulta(id),
+    idExamenLab INT NOT NULL REFERENCES ExamenLaboratorio(id),
+    detalle VARCHAR(255),
+    resultado VARCHAR(500),
+    fechaResultado DATETIME NULL
+);
+DROP TABLE IF EXISTS DetalleProcedimiento;
+CREATE TABLE DetalleProcedimiento(
+    id INT AUTO_INCREMENT PRIMARY KEY ,
+	idConsulta INT NOT NULL REFERENCES Consulta(id),
+    idProcedimiento INT NOT NULL REFERENCES Procedimiento(id),
+    detalle VARCHAR(255),
+    indicacion VARCHAR(255),
+    resultado VARCHAR(500),
+    imagenes BOOL DEFAULT FALSE,
+    directorio varchar(500),
+    fechaResultado DATETIME NULL
+);
+DROP TABLE IF EXISTS MedidaLente;
+CREATE TABLE MedidaLente(
+	id INT AUTO_INCREMENT  PRIMARY KEY,
+    idConsulta INT NOT NULL REFERENCES Consulta(id),
+    -- lejos
+    ODSPHL VARCHAR(6) NULL,
+	ODCYSL VARCHAR(6) NULL,
+	ODAXIL VARCHAR(6) NULL,
+    ODAV VARCHAR(6) NULL,
+
+	OISPHL VARCHAR(6) NULL,
+	OICYSL VARCHAR(6) NULL,
+	OIAXIL VARCHAR(6) NULL,
+    OIAV VARCHAR(6) NULL,
+
+	PDL VARCHAR(3) NULL,
+
+	OBSL VARCHAR(120) NULL,
+    -- cerca
+    ODSPHC VARCHAR(6) NULL,
+	ODCYSC VARCHAR(6) NULL,
+	ODAXIC VARCHAR(6) NULL,
+
+	OISPHC VARCHAR(6) NULL,
+	OICYSC VARCHAR(6) NULL,
+	OIAXIC VARCHAR(6) NULL,
+
+	PDC VARCHAR(3) NULL,
+
+	OBSC VARCHAR(120) NULL,
+
+    preventiva BOOL DEFAULT FALSE
+);
+DROP TABLE IF EXISTS Medicacion;
+CREATE TABLE Medicacion(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    idConsulta INT NOT NULL REFERENCES Consulta(id),
+	idMedicamento INT NOT NULL REFERENCES Medicamento(id),
+	dosis VARCHAR(80) NOT NULL,
+	indicacion VARCHAR(300) NULL,
+    ordenMedica VARCHAR(500) NULL
+);
