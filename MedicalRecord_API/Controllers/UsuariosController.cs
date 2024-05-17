568f3f47a58dab6527ea5e109b306a28a840d487
luhan <n00209455@upn.pe>
2024-05-17 17:53:42 -0500

    feat: getusuario

--- MedicalRecord_API/Controllers/UsuariosController.cs
+++ MedicalRecord_API/Controllers/UsuariosController.cs
@@ -33,9 +33,9 @@ namespace MedicalRecord_API.Controllers
         [ProducesResponseType(StatusCodes.Status102Processing)]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult<Response>> GetAll()
+        public async Task<ActionResult<Response>> GetUsuarios()
         {
-            _logger.LogInformation("{StatusCode}[{HttpStatusCode}]: Handling GET_All request", StatusCodes.Status102Processing, HttpStatusCode.Processing);
+            _logger.LogInformation("{StatusCode}[{HttpStatusCode}]: Handling GETUSUARIOS request", StatusCodes.Status102Processing, HttpStatusCode.Processing);
 
             try
             {
@@ -51,9 +51,49 @@ namespace MedicalRecord_API.Controllers
                 _response.IsExitoso = false;
                 _response.StatusCode = HttpStatusCode.InternalServerError;
                 _response.ErrorMensajes =[ ex.ToString() ];
-                _logger.LogError(ex, "{StatusCode}[{HttpStatusCode}]: Error handling GET_ALL request", StatusCodes.Status500InternalServerError, HttpStatusCode.InternalServerError);
-
+                _logger.LogError("{StatusCode}[{HttpStatusCode}]: Error handling GETUSUARIOS request", StatusCodes.Status500InternalServerError, HttpStatusCode.InternalServerError);
                 return StatusCode(StatusCodes.Status500InternalServerError, _response);
+            }
+        }
+        [HttpGet("Id:int", Name = "GetUsuario")]
+        [ProducesResponseType(StatusCodes.Status102Processing)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(statusCode: StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Response>> GetUsuario(int Id)
+        {
+            _logger.LogInformation("{StatusCode}[{HttpStatusCode}]: Handling GETUSUARIO request", StatusCodes.Status102Processing, HttpStatusCode.Processing);
+
+            try
+            {
+                if (Id == 0)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.IsExitoso = false;
+                    _logger.LogError("{StatusCode}[{HttpStatusCode}]: Error handling GETUSUARIO request", StatusCodes.Status400BadRequest, HttpStatusCode.BadRequest);
+                    return BadRequest(_response);
+                }
+                UsuarioCreateDto usuarioDto = _mapper.Map<UsuarioCreateDto>(await _usuarioRepo.GetEntity(u =>u.Id == Id, false));
+                if (usuarioDto == null)
+                {
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    _response.IsExitoso = false;
+                    _logger.LogError("{StatusCode}[{HttpStatusCode}]: Error handling GETUSUARIO request", StatusCodes.Status404NotFound, HttpStatusCode.NotFound);
+                    return NotFound(_response);
+                }
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.Resultado = usuarioDto;
+                _logger.LogInformation("{StatusCode}[{HttpStatusCode}]: Request handled successfully", StatusCodes.Status200OK, HttpStatusCode.OK);
+
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsExitoso = false;
+                _response.ErrorMensajes = [ex.ToString()];
+                _logger.LogError("{StatusCode}[{HttpStatusCode}]: Error handling GETUSUARIOS request", StatusCodes.Status500InternalServerError, HttpStatusCode.InternalServerError);
+                return StatusCode(StatusCodes.Status500InternalServerError, _response);
+
             }
         }
     }
