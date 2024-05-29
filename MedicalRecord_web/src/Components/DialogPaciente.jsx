e91b0a8286b0049caf8f7be532836d2e0943e786
luhan <n00209455@upn.pe>
2024-05-29 14:27:58 -0500

    dialog paciente

--- MedicalRecord_web/src/Components/DialogPaciente.jsx
+++ MedicalRecord_web/src/Components/DialogPaciente.jsx
@@ -1,5 +1,4 @@
 import {
-  Button,
   Dialog,
   DialogPanel,
   DialogTitle,
@@ -12,11 +11,11 @@ export function DialogPaciente({ isOpen, close }) {
     <Transition appear show={isOpen}>
       <Dialog
         as='div'
-        className='relative z-10 focus:outline-none '
+        className='relative z-10 focus:outline-none'
         onClose={close}
         __demoMode
       >
-        <div className='fixed inset-0 z-10 w-screen overflow-y-auto '>
+        <div className='fixed inset-0 z-10 w-screen '>
           <div className='flex min-h-full items-center justify-center p-4'>
             <TransitionChild
               enter='ease-out duration-300'
@@ -26,25 +25,36 @@ export function DialogPaciente({ isOpen, close }) {
               leaveFrom='opacity-100 transform-[scale(100%)]'
               leaveTo='opacity-0 transform-[scale(95%)]'
             >
-              <DialogPanel className='w-full max-w-xl rounded-xl bg-white/5 p-6 backdrop-blur-2xl '>
+              <DialogPanel className='relative w-80 h-full  max-w-md rounded-xl bg-white/5 p-6 backdrop-blur-2xl'>
+                <button
+                  className='absolute top-2 right-2 text-slate-500  p-1 focus:outline-none hover:text-white'
+                  onClick={close}
+                >
+                  ✕
+                </button>
                 <DialogTitle
                   as='h3'
                   className='text-base/7 font-medium text-white'
                 >
-                  Paciente
+                  Datos - Antecedentes
                 </DialogTitle>
                 <p className='mt-2 text-sm/6 text-white/50'>
-                  Aqui podrá ver todo los detalles del paciente incluido sus
-                  antecedentes
+                  Your payment has been successfully submitted. We’ve sent you
+                  an email with all of the details of your order. Lorem ipsum
+                  dolor, sit amet consectetur adipisicing elit. Sapiente,
+                  consequuntur esse! Obcaecati veniam inventore laudantium dolor
+                  dignissimos recusandae! Veniam doloribus in eum? Omnis iste
+                  perferendis aspernatur minima nulla temporibus eius? Lorem
+                  ipsum dolor sit amet consectetur adipisicing elit. Vel odio et
+                  quis tempora ipsa beatae distinctio velit laborum atque
+                  reprehenderit. Est nihil ipsa maxime molestias, consequatur
+                  neque quas esse corrupti? Lorem ipsum dolor sit amet
+                  consectetur adipisicing elit. Vitae tempore vel cumque,
+                  consequatur officia quibusdam aliquam dolor pariatur cum
+                  recusandae impedit magnam autem harum enim ex! Magni ipsa quae
+                  neque.
                 </p>
-                <div className='mt-4'>
-                  <Button
-                    className='inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white'
-                    onClick={close}
-                  >
-                    Got it, thanks!
-                  </Button>
-                </div>
+                <div className='mt-4'></div>
               </DialogPanel>
             </TransitionChild>
           </div>
